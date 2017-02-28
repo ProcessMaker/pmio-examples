62b84d3ca1d6aacd7acf553f3101351033418c8e
Oleg Khimich <oleg@processmaker.com>
2017-03-01 00:18:59 +0200

    setcookie

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require ".env";
 if($_GET['user']) $_COOKIE['user']=$_GET['user'];
 
 $user = (isset($_COOKIE['user'])?$_COOKIE['user']:'Test');
+setcookie("user", $user, time()+3600);
 $key = $key[$user];
 switch ($user) {
     case 'Alice':
